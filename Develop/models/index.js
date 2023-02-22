47ae77588c7e68ad989547e6fa9980aeb0b6ac80
Khiaotic <112679225+Khiaotic@users.noreply.github.com>
2023-02-21 21:28:53 -0500

    added to comment routes

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,2 +1,29 @@
-///relationships between models. user has many posts/opinions
-/// 
+
+const User = require('./User');
+
+const Comment = require('./Comment');
+
+const BlogPost = require('./BlogPost');
+
+User.hasMany(BlogPost, {
+    foreignKey: 'user_id',
+});
+BlogPost.hasMany(Comment, {
+    foreignKey: 'blogPost_id',
+});
+
+BlogPost.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+////comment belongs to blog post or the user who makes the comment..on the blog post?
+Comment.belongsTo(BlogPost, {
+    foreignKey: 'blogPost_id'
+});
+
+module.exports = {User, Comment, BlogPost}
+
+
+
+
+
